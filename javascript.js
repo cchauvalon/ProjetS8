212b71bb00d76d91fc5dbd230d720eabc9a83a2d
florian <florian.louistherese@gmail.com>
2014-01-30 14:34:23 +0100

    Mon tout premier commit <3

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function maFonction(){
+var elTxt = new Array();
+ 
+//Textes des éléments
+var tabTxt = new Array("Votre texte : ","Vos options : ","La suite : ");
+ 
+for (i=0; i<tabTxt.length; i++)
+{
+    elTxt[i] = document.createTextNode(tabTxt[i]);
+	document.body.appendChild(elTxt[i]); 
+	aaaa
+}
+
+}
